5d1ea39222e025fe853582261c769051a0b4589d
atharvbyadav <atharvbyadav@gmail.com>
2026-03-18 08:58:47 +0000

    updating for win support

--- util/random.c
+++ util/random.c
@@ -3,23 +3,21 @@
 #include <sys/random.h>
 #include <unistd.h>
 
-int zt_random_fill(
-    unsigned char *buf,
-    size_t len
-)
+int zt_random_fill(unsigned char *buf, size_t len)
 {
-    ssize_t r =
-        getrandom(
-            buf,
-            len,
-            0
-        );
-
-    if (r < 0)
+#if defined(_WIN32) || defined(__MINGW32__)
+    #include <windows.h>
+    #include <wincrypt.h>
+    HCRYPTPROV hProv = 0;
+    if (!CryptAcquireContext(&hProv, NULL, NULL,
+            PROV_RSA_FULL, CRYPT_VERIFYCONTEXT))
         return -1;
-
-    if ((size_t)r != len)
-        return -1;
-
-    return 0;
+    BOOL ok = CryptGenRandom(hProv, (DWORD)len, buf);
+    CryptReleaseContext(hProv, 0);
+    return ok ? 0 : -1;
+#else
+    #include <sys/random.h>
+    ssize_t r = getrandom(buf, len, 0);
+    return (r >= 0 && (size_t)r == len) ? 0 : -1;
+#endif
 }
